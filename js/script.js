7556cb0034cd14eb8b4c7c667e2c08cb6899af6b
Jim Poulin <jpoulin587a@gmail.com>
2021-06-26 13:36:40 -0500

    added todo psudo-code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
+/* //establish the elements like this
+
+let submitButton = document.getElementById("submit-button");
+*/
+
+/* // from trivia quiz, adapt to date from the array to the dynamically create the list of breweries.
+
+function populateQuestion () {
+    let currentQuestion = questionArray[questionIndex]
+    questionText.innerText = currentQuestion.quest;
+    currentQuestion.choices.forEach(function(choice, i){
+        //console.log(questionIndex + "questionIndex")
+        //console.log(i);
+        //console.log(choice);
+        let choiceButton = document.createElement("button");  
+        choiceButton.textContent = choice; 
+        choiceButton.onclick = questionClick;
+        answerList.appendChild(choiceButton);
+    })
+
+}
+*/
+
+//These will come from user input.
+let userStartingNumber = "6501";
+let userStartingStreet = "Memorial";
+let userStreetType = "drive"
+let userStartingCity = "houston";
+let UserStartingState = "tx"
+
+
+
+
+
+
+
+
+
 console.log("the link works")
 var requestUrl = 'https://api.openbrewerydb.org/breweries?by_city=san%20diego';
 
